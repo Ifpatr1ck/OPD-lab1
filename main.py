93b3b80eaa4d0de7d40895e6b3c0a778aca78bb2
Denis <ifpatrick@bk.ru>
2023-02-15 23:11:03 +0600

    second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+first_var = float(input("Введите первое число:"))
+second_var = float(input("Введите второе число:"))
+operation = input("Выберите тип операции: '+','-','*','/' : ")
+
+if (operation == '+'):
+    print(first_var + second_var)
+elif(operation == '-'):
+    print(first_var - second_var)
+elif (operation == '*'):
+    print(first_var * second_var)
+elif (operation == '/'):
+    print(first_var / second_var)
+print("Жижа")
